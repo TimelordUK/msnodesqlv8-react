d05d5fc6b90247f0a4f16f8d5cdd9ef113c17f9a
TimelordUK <sjames8888@gmail.com>
2025-06-26 20:37:38 +0100

    clean up webpack in dev

--- start-electron-dev.js
+++ start-electron-dev.js
@@ -1,9 +1,32 @@
 const { spawn } = require('child_process');
 const path = require('path');
 
+let webpack = null;
+let electron = null;
+
+// Cleanup function to ensure all processes are killed
+function cleanup() {
+  console.log('Cleaning up processes...');
+  if (webpack) {
+    webpack.kill('SIGTERM');
+    // Force kill if it doesn't respond to SIGTERM
+    setTimeout(() => {
+      if (webpack && !webpack.killed) {
+        webpack.kill('SIGKILL');
+      }
+    }, 2000);
+  }
+  if (electron) {
+    electron.kill('SIGTERM');
+  }
+  setTimeout(() => {
+    process.exit();
+  }, 3000);
+}
+
 // Start webpack dev server
 console.log('Starting webpack dev server...');
-const webpack = spawn('npm', ['start'], {
+webpack = spawn('npm', ['start'], {
   stdio: 'inherit',
   shell: true
 });
@@ -11,19 +34,18 @@ const webpack = spawn('npm', ['start'], {
 // Wait a bit for webpack to start, then launch electron
 setTimeout(() => {
   console.log('Starting Electron...');
-  const electron = spawn('electron', ['.'], {
+  electron = spawn('electron', ['.'], {
     stdio: 'inherit',
     shell: true,
     env: { ...process.env, NODE_ENV: 'development' }
   });
 
   electron.on('close', () => {
-    webpack.kill();
-    process.exit();
+    cleanup();
   });
 }, 5000);
 
-process.on('SIGINT', () => {
-  webpack.kill();
-  process.exit();
-});
+// Handle various termination signals
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
+process.on('exit', cleanup);
